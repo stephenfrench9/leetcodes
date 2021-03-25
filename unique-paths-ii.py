963a4b9ef195a9e258a6bf796a2027fd787e2582
Stephen French <stephen.french@invitae.com>
2021-03-25 13:01:04 -0700

    unique-pathes-ii

--- unique-paths-ii.py
+++ unique-paths-ii.py
@@ -1,20 +1,31 @@
 from typing import List
 
-
+# https://leetcode.com/problems/unique-paths-ii/
 ### The following will run successfully when dropped into the leetcode editor (python3)
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
         og = obstacleGrid
         rows = len(og)
-        cols = len(og[1])
+        cols = len(og[0])
 
-        ans_grid = [[0 for c in range(cols)] for r in range(rows)]
-        ans_grid[0] = rows * [1]
-        for r in ans_grid:
-            r[0] = 1
+        for row in range(rows):
+            for col in range(cols):
+                if og[row][col] == 1:
+                    og[row][col] = 0
+                # og[row][col] must equal zero
+                else:
+                    if row == 0:
+                        if col == 0:
+                            og[row][col] = 1
+                        else:
+                            og[row][col] = og[row][col-1]
+                    else:
+                        if col == 0:
+                            og[row][col] = og[row-1][col]
+                        else:
+                            og[row][col] = og[row-1][col] + og[row][col-1]
 
-        print(ans_grid)
-
+        return og[-1][-1]
 
 ### The previous will run successfully when dropped into the leetcode editor (python3)
 
@@ -22,4 +33,6 @@ class Solution:
 # https://stackoverflow.com/questions/4056768/how-to-declare-array-of-zeros-in-python-or-an-array-of-a-certain-size
 
 print("it runs")
-Solution.uniquePathsWithObstacles(Solution, [[0, 0, 0], [0, 0, 0], [0, 0, 0]])
+print(Solution.uniquePathsWithObstacles(Solution, [[0, 0, 0], [0, 1, 0], [0, 0, 0]]))
+print(Solution.uniquePathsWithObstacles(Solution, [[0]]))
+print(Solution.uniquePathsWithObstacles(Solution, [[1]]))
