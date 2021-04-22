c2f2e6509cb35792bdf1e30061097d6ec65517ba
Stephen French <stephen.french@invitae.com>
2021-04-22 14:33:39 -0700

    i forgot

--- palindrome-partitioning-loop.py
+++ palindrome-partitioning-loop.py
@@ -1,4 +1,4 @@
-rest = "abcd"  # the set
+rest = "aabcbaa"  # the set
 considered = 0
 parsings = [[""]]  # all the ways to group all the considered elements, where each
 # parsing is a list and an element of parsings, and each parsing is a grouping of all the considered
@@ -17,12 +17,6 @@ rest = rest[1:]  # the set
 parsings = [[cand]]  # the last element of each parsing is the candidate
 
 while rest:
-    print()
-    print("STARTING LOOP W VALUES")
-    print("cand: ", cand)
-    print("rest: ", rest)
-    print("parsings: ", parsings)
-
     considered += 1
     cand = rest[0]
     rest = rest[1:]
@@ -31,17 +25,17 @@ while rest:
     for parsing in parsings:
         if parsing[-1] == parsing[-1][::-1]:
             new_parsing = [group for group in parsing] + [cand]
-            print("new_parsing: ", new_parsing)
-
             new_parsings.append(new_parsing)
 
     for parsing in parsings:
-        print("PARSING", parsing)
-        print("parsing[-1]: ", parsing[-1])
         parsing[-1] = parsing[-1] + cand
 
+    parsings += new_parsings  # this adds many elements to the parent list.
 
-    parsings.append(new_parsings)
-    print("parsings: ", parsings)
+final_parsings = []
+for parsing in parsings:
+    if parsing[-1] == parsing[-1][::-1]:
+        final_parsing = [group for group in parsing]
+        final_parsings.append(final_parsing)
 
-print(parsings)
+print(final_parsings)
