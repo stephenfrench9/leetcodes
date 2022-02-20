88724abbd5a6f3c04fd37d73f02ea78f36d778a4
Stephen French <stephenfrench9@gmail.com>
2022-02-19 20:49:57 -0700

    https://leetcode.com/problems/jump-game/ -- also too slow

--- dynamic/jump-game.py
+++ dynamic/jump-game.py
@@ -4,6 +4,20 @@ from typing import List
 
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
+        soln = [False]*len(nums)
+        soln[0] = True
+
+        for i, max_jump in enumerate(nums):
+            # soln[i] is either reachable or not
+            if soln[i]:
+                for jump in range(1, max_jump+1):
+                    if i + jump >= len(nums):
+                        break
+                    soln[i + jump] = True
+        return soln[-1]
+
+
+    def canJump__too_slow(self, nums: List[int]) -> bool:
         if len(nums) == 1:
             return True
 
