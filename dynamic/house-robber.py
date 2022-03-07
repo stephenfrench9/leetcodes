35c20716eeab487a5c2e22ae6ed84bcaa7697fbf
Stephen French <stephenfrench9@gmail.com>
2022-03-07 13:52:25 -0800

    www.leetcode.com/problems/house-robber/ best path per index

--- dynamic/house-robber.py
+++ dynamic/house-robber.py
@@ -3,6 +3,17 @@ from typing import List
 
 class Solution:
     def rob(self, nums: List[int]) -> int:
+        cost_1 = 0
+        cost_2 = 0
+        cost_3 = 0
+
+        for n in nums:
+            cost = n + max(cost_2, cost_3)
+            cost_1, cost_2, cost_3 = cost, cost_1, cost_2
+
+        return max(cost_1, cost_2)
+
+    def rob_fast_but_wierd(self, nums: List[int]) -> int:
         op = 0
         o = 0
 
